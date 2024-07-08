c1bad315f04f00ed02dd547e59237cf9a51e221b
chiko12m <chiko.dct@gmail.com>
2024-07-08 16:16:06 +0900

    202407081616

---
+++ assets/script/main.js
@@ -0,0 +1,12 @@
+// スライド
+$('.voice__group').slick({
+    infinite: true,
+    dots: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    centerMode: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+});
